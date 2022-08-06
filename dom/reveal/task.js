2ddf9bcbba0bdf128d1ba50e3824ce41174ba9dc
Evgenii1811 <legal_adviser_surgut@rambler.ru>
2022-08-06 12:08:50 +0500

    ADS

---
+++ dom/reveal/task.js
@@ -0,0 +1,19 @@
+// Zadacha 1
+
+"use strict";
+
+const divReveals = document.querySelectorAll(".reveal");
+const viewHeight = window.innerHeight;
+
+const openClass = function () {
+  for (let divReveal of divReveals) {
+    const divRevealTop = divReveal.getBoundingClientRect().top;
+    const divRevealBottom = divReveal.getBoundingClientRect().bottom;
+    if (divRevealBottom > 0 && divRevealTop < viewHeight) {
+      divReveal.classList.add("reveal_active");
+    } else {
+      divReveal.classList.remove("reveal_active");
+    }
+  }
+};
+document.addEventListener("scroll", openClass);
